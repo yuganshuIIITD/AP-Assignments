1ec507257b16c08e6779a454defdeab80a1618ef
Yuganshu kajla <yuganshu20418@iiitd.ac.in>
2021-10-07 17:02:40 +0530

    started

--- Assignment1/main.java
+++ Assignment1/main.java
@@ -1,1 +1,10 @@
+package Assignment1;
+import java.util.Scanner;
+public class main {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+
+    }
+}

efba514e8e79d949bc89f10f2784806219e55653
Yuganshu kajla <yuganshu20418@iiitd.ac.in>
2021-10-17 14:08:07 +0530

    just 3 to go

--- Assignment2/driver.java
+++ Assignment2/driver.java
@@ -1,10 +1,22 @@
 package Assignment2;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class driver {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
+        course C1=new course("AP");
+        instructor I1=new instructor("yuganshu");
+        instructor I2=new instructor("mohit");
+        students S1=new students();
+        S1.setName("Aditi");
+        students S2=new students();
+        S2.setName("Pranav");
+        students S0=new students();
+        S0.setName("Shrugal");
+        C1.course_instructors.add(I1);
+        C1.course_instructors.add(I2);
         while(true){
             System.out.println("Welcome to Backpack");
             System.out.println("1. Enter as instructor");
@@ -22,14 +34,48 @@ public class driver {
                 System.out.println("6. Add Comments");
                 System.out.println("7. Logout");
                 int input = sc.nextInt();
+                System.out.println("Students:");
+                for(int j=0;j<C1.stu.size();j++){
+                    System.out.println(j+ "-"+ C1.stu.get(j).getName());
+                }
+                int choice=sc.nextInt();
+                students temp=C1.stu.get(choice);
                 if(input==7){
                     break;
                 }else if(input==1){
+                    C1.print_slide_mat();
+                    C1.print_vid_mat();
+                    System.out.println("Welcome "+ temp.getName());
+                }else if(input==2){
+                    C1.print_assesment();
+                }else if(input==3){
 
+                }else if(input==4){
+
+                }else if(input==5){
+                    C1.print_comments();
+                    System.out.println("Welcome "+ temp.getName());
+                }else if(input==6){
+                    sc.nextLine();
+                    String com=sc.nextLine();
+                    comment c=new comment();
+                    c.setComment(com);
+                    c.setName(temp.getName());
+                    Date current_Date = new Date();
+                    c.current_Date=current_Date;
+                    C1.commentlist.add(c);
+                    System.out.println("Welcome "+ temp.getName());
                 }
 
-
             }else if(in==1){
+                System.out.println("Instructors: ");
+                for(int k=0;k<C1.course_instructors.size();k++){
+                    System.out.println(k+ "- " + C1.course_instructors.get(k).instructor_name);
+
+                }
+                int choice=sc.nextInt();
+                instructor temp=C1.course_instructors.get(choice);
+                while(true){
                 System.out.println("1. Add class material");
                 System.out.println("2. Add Assesment");
                 System.out.println("3. View Lecture material");
@@ -52,11 +98,95 @@ public class driver {
                         String topic=sc.nextLine();
                         System.out.println("Enter no slides");
                         int no=sc.nextInt();
-                        
-                    }else if(i==2){
+                        sc.nextLine();
+                        lectureslide l=new lectureslide();
+                        l.setNo_of_slides(no);
+                        l.setLecname(topic);
+                        System.out.println("Enter content of slides");
+                        for(int j=0;j<no;j++){
+                            System.out.println("Enter content of slide " + j + ": ");
+                            String content=sc.nextLine();
+                            l.content.add(content);
+                        }
+                        Date current_Date = new Date();
+                        l.current_Date=current_Date;
+                        l.prof=temp.instructor_name;
+                        C1.lec_slide.add(l);
+                        System.out.println("Welcome "+ temp.instructor_name);
 
+                    }else if(i==2){
+                        sc.nextLine();
+                        System.out.println("Enter topic of video: ");
+                        String topic=sc.nextLine();
+                        System.out.println("Enter file name : ");
+                        String filename=sc.nextLine();
+                        lecturevid v=new lecturevid();
+                        v.setFile_name(filename);
+                        v.setTopic(topic);
+                        Date current_Date = new Date();
+                        v.current_Date=current_Date;
+                        v.prof=temp.instructor_name;
+                        C1.lec_video.add(v);
+                        System.out.println("Welcome "+ temp.instructor_name);
                     }
+                }else if(input==2){
+                    System.out.println("1. Add assignment");
+                    System.out.println("2. Add quiz");
+                    int c=sc.nextInt();
+                    if(c==1){
+                        sc.nextLine();
+                        System.out.println("Enter problem statement : ");
+                        String problem_statement=sc.nextLine();
+                        System.out.println("Enter marks  : ");
+                        int marks=sc.nextInt();
+                        assignment a=new assignment();
+                        a.status="open";
+                        a.setMarks(marks);
+                        a.setProblem_statement(problem_statement);
+                        C1.ass.add(a);
+                        System.out.println("Welcome "+ temp.instructor_name);
+
+
+                    }else if(c==2){
+                        sc.nextLine();
+                        System.out.println("Enter quiz question : ");
+                        String question=sc.nextLine();
+                        quiz q=new quiz();
+                        q.setStatus("open");
+                        q.setQuestion(question);
+                        C1.quizs.add(q);
+                        System.out.println("Welcome "+ temp.instructor_name);
+                    
+                    }
+                }else if(input==3){
+                    C1.print_slide_mat();
+                    C1.print_vid_mat();
+                    System.out.println("Welcome "+ temp.instructor_name);
+                }else if(input==4){
+                    C1.print_assesment();
+                    System.out.println("Welcome "+ temp.instructor_name);
+                }else if(input==5){
+
+                }else if(input==6){
+                    System.out.println("list of open assesments : ");
+                    C1.close_assement();
+                    System.out.println("Welcome "+ temp.instructor_name);
+                }else if(input==7){
+                    C1.print_comments();
+                    System.out.println("Welcome "+ temp.instructor_name);
+
+                }else if(input==8){
+                    sc.nextLine();
+                    String com=sc.nextLine();
+                    comment c=new comment();
+                    c.setComment(com);
+                    c.setName(temp.instructor_name);
+                    Date current_Date = new Date();
+                    c.current_Date=current_Date;
+                    C1.commentlist.add(c);
+                    System.out.println("Welcome "+ temp.instructor_name);
                 }
+            }
             
             }
             
--- Assignment2/instructor.java
+++ Assignment2/instructor.java
@@ -1,5 +1,8 @@
 package Assignment2;
 
-public class instructor {
+public class instructor extends participants {
     String instructor_name;
+    instructor(String n){
+        instructor_name=n;
+    }
 }
